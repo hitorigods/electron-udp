dfe2265315644154bc934e8123932c0482d54b9e
hitorigods <hitorigods1124@gmail.com>
2023-08-16 21:40:30 +0900

    ソース整理

--- src/main/index.js
+++ src/main/index.js
@@ -1,9 +1,9 @@
 const { app, BrowserWindow, ipcMain, Menu } = require('electron');
 const path = require('path');
 const fs = require('fs');
-const dgram = require('dgram'); // UDP通信用モジュールを追加
+const dgram = require('dgram');
 
-//メニューバー内容
+/** メニューバー内容 */
 let template = [
 	{
 		label: 'アプリ',
@@ -51,17 +51,21 @@ let template = [
 let mainWindow;
 
 app.on('ready', () => {
-	//設定ファイル
-	const configPath = path.join(__dirname, '../src/config.json');
+	/** 設定ファイル */
+	const configPath =
+		process.env.NODE_ENV === 'development'
+			? path.join(__dirname, '../renderer/config.json')
+			: path.join(process.resourcesPath, './app/config.json');
 	const configData = fs.readFileSync(configPath, 'utf-8');
 	const config = JSON.parse(configData);
 
-	//メニューバー設置
+	/** メニューバー設置 */
 	const menu = Menu.buildFromTemplate(template);
 	Menu.setApplicationMenu(menu);
 
+	/** ウィンドウ設定 */
 	mainWindow = new BrowserWindow({
-		width: 1400, // ウィンドウ幅を1400に設定
+		width: 1400,
 		height: 800,
 		webPreferences: {
 			nodeIntegration: false,
@@ -70,12 +74,15 @@ app.on('ready', () => {
 		},
 	});
 
-	// 開発モードでウィンドウを開く
 	if (process.env.NODE_ENV === 'development') {
-		mainWindow.webContents.openDevTools(); // DevToolsを自動で開く
+		mainWindow.webContents.openDevTools();
 	}
 
-	mainWindow.loadFile(path.join(__dirname, '../src', 'index.html'));
+	mainWindow.loadFile(
+		process.env.NODE_ENV === 'development'
+			? path.join(__dirname, '../renderer/index.html')
+			: path.join(process.resourcesPath, './app/index.html')
+	);
 
 	mainWindow.webContents.on('did-finish-load', () => {
 		mainWindow.webContents.send('config-data', config);
@@ -83,23 +90,23 @@ app.on('ready', () => {
 });
 
 app.on('activate', () => {
-	// macOSでは、ユーザがドックアイコンをクリックしたとき、
-	// そのアプリのウインドウが無かったら再作成するのが一般的です。
 	if (BrowserWindow.getAllWindows().length === 0) {
 		createWindow();
 	}
 });
 
 ipcMain.on('drag-coordinates', (event, coordinates) => {
-	// UDP通信処理
-	const { serverAddress, serverPort } = require('../src/config.json');
+	/** UDP通信処理 */
+	const { serverAddress, serverPort } = require(process.env.NODE_ENV === 'development'
+		? path.join(__dirname, '../renderer/config.json')
+		: path.join(process.resourcesPath, './app/config.json'));
 
-	// UDP通信で座標データを送信
+	/** 座標データを送信 */
 	const client = dgram.createSocket('udp4');
 	const message = JSON.stringify(coordinates);
 
-	client.send(message, serverPort, serverAddress, (err) => {
-		console.log(`message=${serverAddress}:${serverPort}`, message);
+	client.send(message, 0, message.length, serverPort, serverAddress, (err) => {
+		console.log(message, 0, message.length, serverPort, serverAddress);
 
 		if (err) {
 			console.error('Error sending message:', err);
